5d9d1ea76b9312c4a28011a6110c478672595b3b
KonH <konh@yandex.ru>
2017-05-20 14:45:31 +0700

    AbstractFactory pattern;

---
+++ CreationalPatterns/AbstractFactory.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace DesignPatterns.CreationalPatterns {
+
+	class AbstractFactoryPattern : Pattern {
+
+		public AbstractFactoryPattern() : base("AbstractFactory") { }
+
+		public override void Test() {
+			TestFactory(new SimpleLoggerFactory());
+			TestFactory(new ExtendedLoggerFactory());
+			TestFactory(new TemplateLoggerFactory<CustomLogger>());
+		}
+
+		void TestFactory(LoggerFactory loggerFactory) {
+			var logger = loggerFactory.Create();
+			logger.WriteLine($"{GetShortName(loggerFactory)} create {GetShortName(logger)}");
+		}
+
+		string GetShortName<T>(T obj) {
+			return obj.GetType().Name.ToString();
+		}
+
+		abstract class LoggerFactory {
+
+			public abstract Logger Create();
+		}
+
+		class SimpleLoggerFactory : LoggerFactory {
+
+			public override Logger Create() {
+				return new SimpleLogger();
+			}
+		}
+
+		class ExtendedLoggerFactory : LoggerFactory {
+
+			public override Logger Create() {
+				return new ExtendedLogger();
+			}
+		}
+
+		class TemplateLoggerFactory<T> : LoggerFactory where T: Logger {
+
+			public override Logger Create() {
+				return Activator.CreateInstance<T>();
+			}
+		}
+
+		abstract class Logger {
+
+			public abstract void WriteLine(string content);
+		}
+
+		class SimpleLogger : Logger {
+
+			public override void WriteLine(string content) {
+				Console.WriteLine(content);
+			}
+		}
+
+		class ExtendedLogger : Logger {
+
+			public override void WriteLine(string content) {
+				var dt = DateTime.Now;
+				Console.WriteLine($"{dt.ToLocalTime()}: {content}");
+			}
+		}
+
+		class CustomLogger : Logger {
+
+			int _index = 0;
+
+			public override void WriteLine(string content) {
+				Console.WriteLine($"{_index}: {content}");
+				_index++;
+			}
+		}
+	}
+}
---
+++ Pattern.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns {
+
+	abstract class Pattern {
+		public string Name { get; private set; }
+
+		public Pattern(string name) {
+			Name = name;
+		}
+
+		public abstract void Test();
+	}
+}
